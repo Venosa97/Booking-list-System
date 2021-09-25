777c884c2f3026db4f2b642e401b4a43d215cb76
Lenovo <costivn@gmail.com>
2021-09-25 14:05:46 +0300

    up

--- Java - Modul II/src/exClasassssssssssss/BinarySearch.java
+++ Java - Modul II/src/exClasassssssssssss/BinarySearch.java
@@ -1,7 +1,30 @@
 package exClasassssssssssss;
 
 public class BinarySearch {
-
+	
+	  public int search(int[] nums, int target) {
+	        int left = 0;
+	        int right = nums.length - 1;
+	        int half = (left + right)/2;
+	        
+	        
+	        while (true){
+	            if (nums[half] == target){
+	                return half;
+	            }
+	            if (nums[half] < target){
+	                left = half + 1;
+	            } else if (nums[half] > target){
+	                right = half - 1;
+	            }
+	            half = (left + right) / 2;
+	            if (left > right){
+	               break;
+	            }
+	        }
+	        return -1;
+	    }
+	  
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
