42553819ac8fde0cd26e8cd9d0bfa031355eb5fd
Venosa97 <72497352+Venosa97@users.noreply.github.com>
2021-10-01 16:53:15 +0300

    Delete Java - Modul II/src/enumTypes directory

--- Java - Modul II/src/enumTypes/Rank.java
+++
@@ -1,6 +0,0 @@
-package enumTypes;
-
-public enum Rank {
-	TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN,
-	ACE, JACK, QUEEN, KING;
-}
--- Java - Modul II/src/enumTypes/Suits.java
+++
@@ -1,5 +0,0 @@
-package enumTypes;
-
-public enum Suits {
-	CLUBS, DIAMONDS, HEARTS, SPADES;
-}
