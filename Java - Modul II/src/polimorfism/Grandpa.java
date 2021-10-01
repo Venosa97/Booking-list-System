ea1f5285a69a214c4a783ab9f87da7875bb0535f
Venosa97 <72497352+Venosa97@users.noreply.github.com>
2021-10-01 16:53:59 +0300

    Delete Java - Modul II/src/polimorfism directory

--- Java - Modul II/src/polimorfism/Grandpa.java
+++
@@ -1,8 +0,0 @@
-package polimorfism;
-
-public class Grandpa {
-	
-	public void saySomething() {
-		System.out.println("I need my pills");
-	}
-}
--- Java - Modul II/src/polimorfism/Son.java
+++
@@ -1,9 +0,0 @@
-package polimorfism;
-
-public class Son extends Grandpa {
-	
-	@Override
-	public void saySomething() {
-		System.out.println("I have one kid and he's grandpa needs pills.");
-	}
-}
